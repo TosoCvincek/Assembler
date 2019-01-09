3812e62971f73465b2e99826e76be7564b0ae729
Tomáš Cvinček <45951877+TosoCvincek@users.noreply.github.com>
2019-01-09 16:54:15 +0100

    Add files via upload

--- main.asm
+++ main.asm
@@ -2,68 +2,32 @@ TITLE MASM Template						(main.asm)
 
 INCLUDE Irvine32.inc
 .data
-	Draci DB 3,6,12
-	DB 5,8,14
-	DB 10,9,15
-	DB 6, 12, 18
-	DB 7, 15, 20
-	DB 4, 16, 21
-	Pocet DB 0;
-	DlzkaRiadku EQU sizeof Draci;
+Array DW 2181, 57, 1122;
+Cislo DW 1122;
+Answer DB 2 dup(0);
 .code
-UlohaC PROC USES EAX EBX ECX EDX EDI ESI;
-	xor edi, edi;
-	xor eax, eax;
-	mov esi, 1;
-	mov ecx, 6;
+vysledok PROC USES ECX EAX
+mov ecx, lengthofArray;
 Cyklus:
-	mov al, Draci[edi + esi];
-	cmp al, 12;
-	jbe Schopny;
-	jmp Koniec;
-Schopny:
-	mov al, Draci[edi + esi + 1];
-	cmp al, 14;
-	jae Pripocitaj;
-	jmp Koniec;
-Pripocitaj:
-	mov bl, Draci[edi + esi - 1];
-	add Pocet, bl;
-Koniec:
-	add edi, DlzkaRiadku;
+	cmp ax,Array[2*ecx - 2];
+	je Hodnota;
+	mov Answer,'n';
 	loop Cyklus;
-	ret;
-UlohaC ENDP;
-UlohaA PROC USES EAX EBX ECX EDX EDI ESI;
-		xor edi, edi;
-	xor eax, eax;
-	mov esi, 1;
-	mov ecx, 6;
-Cyklus:
-	mov al, Draci[edi + esi];
-	cmp al, 16;
-	jbe Schopny;
 	jmp Koniec;
-Schopny:
-	mov al, Draci[edi + esi + 1];
-	cmp al, 16;
-	jae Pripocitaj;
-	jmp Koniec;
-Pripocitaj:
-	inc Pocet;
+Hodnota:
+	mov Answer,'a';
 Koniec:
-	add edi, DlzkaRiadku;
-	loop Cyklus;
 	ret;
-UlohaA ENDP;
+
+
+vysledok ENDP
+
+
 main PROC
-	INVOKE UlohaA;
-	movzx eax, Pocet;
-	call WriteInt;
-	mov Pocet, 0;
-	INVOKE UlohaC;
-	movzx eax, Pocet;
-	call WriteInt;
+mov ax, Cislo;
+call vysledok;
+mov edx, offset Answer;
+call WriteString;
 	exit
 main ENDP
 
